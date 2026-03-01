7867058798221a117ad1379cac53776e067336de
ParrellaEmanuele <emanueleparrella9@gmail.com>
2026-03-01 01:26:49 +0100

    lezione 25/02

--- main.py
+++ main.py
@@ -1,59 +1,91 @@
+# Scriviamo un codice python che modelli un semplice
+# gestionale aziendale. Dovremo prvedere la possibilità di
+# definire entità che modellano i prodotti, i clienti,
+# offrire interfacce per calcolare i prezzi, eventualmente
+# scontati, ...
+
 class Prodotto:
+    aliquota_iva = 0.22 #variabile di classe -- ovvero è la stessa per tutte le istanze che verranno create.
 
-    aliquota_iva = 0.22 # variabile di classe
-
-    def __init__(self, name: str, price: float, quantity: float, supplier: str):
+    def __init__(self, name: str, price: float, quantity: int, supplier = None):
         self.name = name
+        self._price = None
         self.price = price
         self.quantity = quantity
         self.supplier = supplier
 
     def valore_netto(self):
-        return self.price * self.quantity
+        return self._price*self.quantity
 
     def valore_lordo(self):
         netto = self.valore_netto()
-        lordo = netto * (1 + self.aliquota_iva)
+        lordo = netto*(1+self.aliquota_iva)
         return lordo
 
     @classmethod
     def costruttore_con_quantità_uno(cls, name: str, price: float, supplier: str):
-        return cls(name, price, 1, supplier)
+        cls(name, price, 1, supplier)
 
     @staticmethod
     def applica_sconto(prezzo, percentuale):
-        return prezzo * (1 - percentuale)
+        return prezzo*(1-percentuale)
 
     @property
-    def price(self):
-        return self.price
-
+    def price(self): # eq. getter
+        return self._price
     @price.setter
     def price(self, valore):
         if valore < 0:
             raise ValueError("Attenzione, il prezzo non può essere negativo.")
-        self.price = valore
+        self._price = valore
 
-myProduct1 = Prodotto(name = "Laptop", price = 1200.0, quantity = 12, supplier = "ABC")
 
-print(f"Nome prodotto: {myProduct1.name} - prezzo: {myProduct1.price}")
 
-print(f"Il totale lordo di myProduct1 è {myProduct1.valore_lordo()}")
-p3 = Prodotto.costruttore_con_quantità_uno(name = "Auricolari", price = 200.0, supplier = "DEF")
-print(f"Prezzo scontato di myProduct1 {Prodotto.applica_sconto(myProduct1.price, 0.22)}")
+myproduct1 = Prodotto(name = "Laptop", price = 1200.0, quantity=12, supplier="ABC")
 
-myProduct2 = Prodotto(name = "Mouse", price = 10, quantity = 25, supplier = "CDE")
-print(f"Nome prodotto: {myProduct2.name} - prezzo: {myProduct2.price}")
+print(f"Nome prodotto: {myproduct1.name} - prezzo: {myproduct1.price}")
 
+print(f"Il totale lordo di myproduct1 è {myproduct1.valore_lordo()}") #uso un metodo di istanza
+p3 = Prodotto.costruttore_con_quantità_uno("Auricolari", 200.0, "ABC") #Modo per chiamare un metodo di classe.
+print(f"Prezzo scontato di myproduct1 {Prodotto.applica_sconto(myproduct1.price, 0.15)}")#Modo per chiamare un metodo statico.
+
+myproduct2 = Prodotto("Mouse", 10, 25, "CDE")
+print(f"Nome prodotto: {myproduct2.name} - prezzo: {myproduct2.price}")
+
+print(f"Valore lordo di myproduct1: {myproduct1.valore_lordo()}")
+Prodotto.aliquota_iva = 0.24
+print(f"Valore lordo di myproduct1: {myproduct1.valore_lordo()}")
+
+#Scrivere una classe Cliente che abbia i campi "nome", "email", "categoria" ("Gold", "Silver", "Bronze").
+#vorremmo che questa classe avesse un metodo che chiamiamo "descrizione"
+# che deve restituire una stringa formattata ad esempio
+#"Cliente Fulvio Bianchi (Gold) - fulvio@google.com"
+
+#Si modifichi la classe cliente in maniera tale che la proprietà categoria sia "protetta"
+# e accetti solo ("Gold", "Silver", "Bronze")
 
 class Cliente:
-    def __init__(self, nome: str, mail: str, categoria: str):
+    def __init__(self, nome, mail, categoria):
         self.nome = nome
         self.mail = mail
+        self._categoria = None
         self.categoria = categoria
 
-    def descrizione(self):
+    @property
+    def categoria(self):
+        return self._categoria
+
+    @categoria.setter
+    def categoria(self, categoria):
+        categorie_valide = {"Gold", "Silver", "Bronze"}
+        if categoria not in categorie_valide:
+            raise ValueError("Attenzione, categoria non valida. Scegliere fra Gold, Silver, Bronze")
+        self._categoria = categoria
+
+    def descrizione(self): #to_string
+        # "Cliente Fulvio Bianchi (Gold) - fulvio@google.com"
         return f"Cliente {self.nome} ({self.categoria}) - {self.mail}"
 
-c1 = Cliente(nome = "Mario Bianchi", mail = "mario.bianchi@polito.it", categoria = "Gold")
+c1 = Cliente("Mario Bianchi", "mario.bianchi@polito.it", "Gold")
+c2 = Cliente("Carlo Masone", "carlo.masone@polito.it", "Platinum")
 print(c1.descrizione())
